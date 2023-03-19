70f50c27fce03b83e5a3de51fc9e691f3b41c644
anarmh <anarmh@code.edu.az>
2023-03-19 01:01:34 -0700

    add html down-navbar

---
+++ assets/js/home.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // $(".dropdowm").click(function(){
+    //     $(this).find(".dropdown-menu").toggle();
+    // })
+    // $('.dropdown-menu a').click(function() {
+    //     var text = $(this).text();
+    //     $('.down a').text(text);
+    //     $('.dropdown-menu').hide();
+    //   });
+})
