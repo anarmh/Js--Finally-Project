4e78ad1abbf7b2dc02eed6ba561850b17c4bc9d5
anarmh <anarmh@code.edu.az>
2023-03-20 12:12:37 -0700

    add slider js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,10 +1,32 @@
 $(document).ready(function(){
-    // $(".dropdowm").click(function(){
-    //     $(this).find(".dropdown-menu").toggle();
-    // })
-    // $('.dropdown-menu a').click(function() {
-    //     var text = $(this).text();
-    //     $('.down a').text(text);
-    //     $('.dropdown-menu').hide();
-    //   });
+    
+  
+        $(".right").on("click", function () {
+            let current = $(".active-img");
+            let next = current.next();
+            if (next.length) {
+              current.removeClass("active-img");
+              next.addClass("active-img");
+            } else {
+              current.removeClass("active-img");
+              $("img:first-child").addClass("active-img");
+            }
+          });
+          $(".left").on("click", function () {
+            let current = $(".active-img");
+            let prev = current.prev();
+            if (prev.length) {
+              current.removeClass("active-img");
+              prev.addClass("active-img");
+            } else {
+              current.removeClass("active-img");
+              $("img:last-child").addClass("active-img");
+            }
+          });
+        
+    
+     
+
+
+   
 })
