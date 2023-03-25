f1fc62efe52cb5202bc0f1c2af51c564520c1131
anarmh <anarmh@code.edu.az>
2023-03-25 01:59:05 -0700

    add slider section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
     
   
-        $(".right").on("click", function () {
-            let current = $(".active-img");
-            let next = current.next();
-            if (next.length) {
-              current.removeClass("active-img");
-              next.addClass("active-img");
-            } else {
-              current.removeClass("active-img");
-              $("img:first-child").addClass("active-img");
-            }
-          });
-          $(".left").on("click", function () {
-            let current = $(".active-img");
-            let prev = current.prev();
-            if (prev.length) {
-              current.removeClass("active-img");
-              prev.addClass("active-img");
-            } else {
-              current.removeClass("active-img");
-              $("img:last-child").addClass("active-img");
-            }
-          });
+        // $(".right").on("click", function () {
+        //     let current = $(".active-img");
+        //     let next = current.next();
+        //     if (next.length) {
+        //       current.removeClass("active-img");
+        //       next.addClass("active-img");
+        //     } else {
+        //       current.removeClass("active-img");
+        //       $("img:first-child").addClass("active-img");
+        //     }
+        //   });
+        //   $(".left").on("click", function () {
+        //     let current = $(".active-img");
+        //     let prev = current.prev();
+        //     if (prev.length) {
+        //       current.removeClass("active-img");
+        //       prev.addClass("active-img");
+        //     } else {
+        //       current.removeClass("active-img");
+        //       $("img:last-child").addClass("active-img");
+        //     }
+        //   });
         
     
      
