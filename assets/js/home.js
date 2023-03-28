93dac35cc075ede9eeb505187d5c41fb945dab18
anarmh <anarmh@code.edu.az>
2023-03-28 02:08:59 -0700

    add trending products section

--- assets/js/home.js
+++ assets/js/home.js
@@ -74,6 +74,41 @@ $('.responsive').slick({
         ]
       });
 
+      $('.product').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
    
     
   
@@ -103,6 +138,31 @@ menues.forEach(menu => {
       
     });
   
+  })
+});
+
+
+let items=document.querySelectorAll("#trending-products .list-products .item");
+
+let cards=document.querySelectorAll("#trending-products .cards .product");
+
+
+items.forEach(item => {
+  
+  item.addEventListener("click",function(e){
+    e.preventDefault()
+    document.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+
+    cards.forEach(card => {
+      if(this.getAttribute("data-id")==card.getAttribute("data-id")){
+        card.classList.remove("d-none")
+      }
+
+      else{
+        card.classList.add("d-none");
+      }
+    });
   })
 });
 
