d8636e52e26978efed2ac7bf6702eb78ab1d7bd3
anarmh <anarmh@code.edu.az>
2023-03-29 22:53:57 -0700

    add update gome js

--- assets/js/home.js
+++ assets/js/home.js
@@ -158,96 +158,45 @@ $(document).ready(function () {
   
   });
   
+  let menues = $("#tab-carousel .tab-menu .item");
+
+  menues.on("click", function(e) {
+    e.preventDefault();
+    $(".active-menu").removeClass("active-menu");
+    $(this).addClass("active-menu");
   
-  let menues=document.querySelectorAll("#tab-carousel .tab-menu .item")
-      
-  
-  menues.forEach(menu => {
-    menu.addEventListener("click",function(e){
-      e.preventDefault();
-      document.querySelector(".active-menu").classList.remove("active-menu");
-      this.classList.add("active-menu");
-      
-
-      let id=$(menu).data("id")
-
-      $(`.responsive`).addClass("d-none")
-
-      $(`.responsive[data-id=`+id+`]`).removeClass("d-none")
-      $(`.responsive[data-id=`+id+`]`).slick("setPosition")
-
-
-      // contents.forEach(content => {
-      //   if(this.getAttribute("data-id")==content.getAttribute("data-id")){
-          
-      //     content.classList.remove("d-none")
-      //   }
-      //   else{
-      //     content.classList.add("d-none")
-      //   }
-  
-        
-      // });
-    
-    })
+    let id = $(this).data("id");
+    $(".responsive").addClass("d-none");
+    $(`.responsive[data-id=${id}]`).removeClass("d-none").slick("setPosition");
   });
-  
-  
-  let items=document.querySelectorAll("#trending-products .list-products .item");
-  
-  items.forEach(item => {
-    
-    item.addEventListener("click",function(e){
-      e.preventDefault()
-      document.querySelector(".active-tab").classList.remove("active-tab");
-      this.classList.add("active-tab");
-  
-      let id = $(item).data('id');
-  
-      $('.product').addClass('d-none');
-      $('.product[data-id='+id+']').removeClass('d-none');
+ 
+  let items = $("#trending-products .list-products .item");
 
-      $('.product[data-id='+id+']').slick('setPosition');
+  items.on("click", function(e) {
+    e.preventDefault();
+    $(".active-tab").removeClass("active-tab");
+    $(this).addClass("active-tab");
   
-      // cards.forEach(card => {
-      //   if(this.getAttribute("data-id")==card.getAttribute("data-id")){
-      //     card.classList.remove("d-none")
-      //   }
-      //   else{
-      //     card.classList.add("d-none");
-      //   }
-      // });
-    })
-    
+    let id = $(this).data("id");
+    $(".product").addClass("d-none");
+    $(`.product[data-id=${id}]`).removeClass("d-none").slick("setPosition");
   });
   
 
-  let sellingProducts=document.querySelectorAll("#top-selling .list-products .item");
+ 
   
 
-  sellingProducts.forEach(product => {
-    
+ 
+  let sellingProducts = $("#top-selling .list-products .item");
 
-    product.addEventListener("click",function(){
-
-      document.querySelector(".active-product").classList.remove("active-product")
-      this.classList.add("active-product");
+  sellingProducts.on("click", function(e) {
+    e.preventDefault();
+    $(".active-product").removeClass("active-product");
+    $(this).addClass("active-product");
   
-  
-      
-      let id = $(product).data('id');
-    
-      $('.product-selling').addClass('d-none');
-      $('.product-selling[data-id='+id+']').removeClass('d-none');
-  
-      $('.product-selling[data-id='+id+']').slick('setPosition');
-
-
-
-
-    })
-   
-
+    let id = $(this).data("id");
+    $(".product-selling").addClass("d-none");
+    $(`.product-selling[data-id=${id}]`).removeClass("d-none").slick("setPosition");
   });
 
 
